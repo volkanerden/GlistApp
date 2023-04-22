3a5f2abfd0d02ca25dd95ffff765162cf9a6112d
Volkan Erden <e.volkanerden@gmail.com>
2023-04-22 14:07:50 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@
 
 int main(int argc, char **argv) {
 
-	gStartEngine(new gApp(), "GlistApp", G_WINDOWMODE_APP, 960, 540);
+	gStartEngine(new gApp(), "GlistApp", G_WINDOWMODE_APP, 768, 768);
 
 	return 0;
 }
